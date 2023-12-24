7ed8a8fbc73de92d5f6b0a68efe8a7a3f78f68d6
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-12-24 12:58:22 +0530

    expense scripts

--- frontend.sh
+++ frontend.sh
@@ -10,10 +10,11 @@ rm -rf /usr/share/nginx/html/* >>/tmp/expense.log
 echo download frontend code
 curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>/tmp/expense.log
 
-echo extracting frontend code
 cd /usr/share/nginx/html >>/tmp/expense.log
+
+echo extracting frontend code
 unzip /tmp/frontend.zip >>/tmp/expense.log
 
 echo starting nginx service
 systemctl enable nginx >>/tmp/expense.log
-systemctl start nginx >>/tmp/expense.log
+systemctl restart nginx >>/tmp/expense.log
