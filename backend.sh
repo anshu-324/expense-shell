65138f1dbc5a3f0d8a49f6f0aabe31ce4c4c30b7
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-12-25 22:30:57 +0530

    expense scripts

--- backend.sh
+++ backend.sh
@@ -1,37 +1,37 @@
-log_file = /tmp/expense.log
+log_file=/tmp/expense.log
 echo install nodejs repos
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash >>$log_file
 
 echo install nodejs
-dnf install nodejs -y
+dnf install nodejs -y >>$log_file
 
 echo copy backend service file
-cp backend.service /etc/systemd/system/backend.service
+cp backend.service /etc/systemd/system/backend.service >>$log_file
 
 echo add application user
-useradd expense
+useradd expense >>$log_file
 
 echo clean app content
-rm -rf /app
-mkdir /app
-cd /app
+rm -rf /app >>$log_file
+mkdir /app >>$log_file
+cd /app >>$log_file
 
 echo download app content
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >>$log_file
 
 echo extract app content
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip >>$log_file
 
 echo download dependencies
-npm install
+npm install >>$log_file
 
 echo start backend service
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
+systemctl daemon-reload >>$log_file
+systemctl enable backend >>$log_file
+systemctl start backend >>$log_file
 
 echo install mysql client
-dnf install mysql -y
+dnf install mysql -y >>$log_file
 
 echo load schema
-mysql -h mysql.malleswaridevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql.malleswaridevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
