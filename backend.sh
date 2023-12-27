7bab01e9cafd157503ed02fc6d522f781bf01af2
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-12-27 21:26:24 +0530

    expense scripts

--- backend.sh
+++ backend.sh
@@ -1,43 +1,19 @@
-#source common.sh
-component=backend
-#echo install nodejs repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
-
-#echo install nodejs
 dnf install nodejs -y
-
-
-#echo copy backend service file
-cp backend.service /etc/systemd/system/backend.service
-
-
-#echo add application user
 useradd expense
 
-
-#echo clean app content
-rm -rf /app
-
 mkdir /app
+
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
+unzip /tmp/backend.zip
 
-download_and_extract
-
-
-#echo download dependencies
+cd /app
 npm install
-
-
-#echo start backend service
 systemctl daemon-reload
+
 systemctl enable backend
 systemctl start backend
 
-
-#echo install mysql client
 dnf install mysql -y
-
-
-#echo load schema
-mysql -h mysql.malleswaridevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pExpenseApp@1 < /app/schema/backend.sql
