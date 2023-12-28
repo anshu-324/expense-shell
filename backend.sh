4aff0329a122cad8a1695975310c2b580673bf38
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-12-28 13:12:14 +0530

    expense scripts

--- backend.sh
+++ backend.sh
@@ -1,27 +1,33 @@
 source common.sh
+echo install nodejs repo
 dnf module disable nodejs -y &>>$log_file
 dnf module enable nodejs:18 -y &>>$log_file
 
+echo install nodejs
 dnf install nodejs -y &>>$log_file
-
+echo copy backend service file
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
+echo add application user
 useradd expense
 
+echo clean app content
 rm -rf /app &>>$log_file
 mkdir /app
-
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
-cd /app
-unzip /tmp/backend.zip &>>$log_file
-
 cd /app
 
+download_and_extract
+
+echo download dependencies
 npm install &>>$log_file
 
+echo start backend service
 systemctl daemon-reload
 
 systemctl enable backend
 systemctl start backend
 
+echo install mysql client
 dnf install mysql -y &>>$log_file
+
+echo load schema
 mysql -h mysql.malleswaridevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
