71d8407890501464c662f3072163786704e5722f
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-12-30 11:30:21 +0530

    expense scripts

--- backend.sh
+++ backend.sh
@@ -4,37 +4,41 @@ echo install nodejs repo
 dnf module disable nodejs -y &>>$log_file
 dnf module enable nodejs:18 -y &>>$log_file
 if [ $? -eq 0 ]; then
-  echo success
+  echo -e "\e[32msuccess\e[0m"
 else
-  echo failed
+  echo -e"\e[31mfailed\e[0m"
 fi
 
 echo install nodejs
 dnf install nodejs -y &>>$log_file
 if [ $? -eq 0 ]; then
-  echo success
+  echo -e "\e[32msuccess\e[0m"
 else
-  echo failed
+  echo -e"\e[31mfailed\e[0m"
 fi
 
 echo copy backend service file
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
 if [ $? -eq 0 ]; then
-  echo success
+  echo -e "\e[32msuccess\e[0m"
 else
-  echo failed
+  echo -e"\e[31mfailed\e[0m"
 fi
 echo add application user
 useradd expense &>>$log_file
 if [ $? -eq 0 ]; then
-  echo success
+  echo -e "\e[32msuccess\e[0m"
 else
-  echo failed
+  echo -e"\e[31mfailed\e[0m"
 fi
 
 echo clean app content
 rm -rf /app &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32msuccess\e[0m"
+else
+  echo -e"\e[31mfailed\e[0m"
+fi
 mkdir /app
 cd /app
 
